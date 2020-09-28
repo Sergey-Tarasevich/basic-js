97086aa154121132d89f6afd3fba352f748e2025
Sergey Tarasevich <70640645+Sergey-Tarasevich@users.noreply.github.com>
2020-09-28 18:52:21 +0300

    feat: add solution to task countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
+    let numCats = 0;
 
-};
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+
+            if (matrix[i][j] === '^^') {
+                numCats++
+            }
+        }
+    }
+    return numCats;
+}
