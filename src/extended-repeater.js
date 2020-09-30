4047b76f9fe4e5a927ee768fded648f8d4dcf199
Sergey Tarasevich <70640645+Sergey-Tarasevich@users.noreply.github.com>
2020-09-30 17:43:50 +0300

    feat: add solution => task ext.-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    let result = ''
+    this.str = toString(str) //str это строка, которая будет повторена
+
+    let {
+        repeatTimes = 1,
+            separator = '+',
+            addition = '',
+            additionRepeatTimes = 1,
+            additionSeparator = '|',
+    } = options
+
+
+
+    for (let i = 0; i < repeatTimes; i++) {
+        result += str
+        for (let j = 0; j < additionRepeatTimes; j++) {
+            result += addition
+            if (j + 1 < additionRepeatTimes) {
+                result += additionSeparator
+            }
+        }
+        if (i + 1 < repeatTimes) {
+            result += separator
+        }
+    }
+    return result
+}
